a3c73b78c126a635191262f3782f8ee15d6a59fe
Administrator <Administrator@UOBJGAPDKFQ3HY3>
2020-04-13 08:58:22 +0700

    nothing

---
+++ src/doan_phandangnhatlinh_qlns_test/DoAn_PhanDangNhatLinh_QLNS_test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doan_phandangnhatlinh_qlns_test;
+
+/**
+ *
+ * @author Administrator
+ */
+public class DoAn_PhanDangNhatLinh_QLNS_test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
